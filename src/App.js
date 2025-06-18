59ca16f9b27c073fd46b321e271778d728fb8f55
edmondhabimana <edmond.habimana@yahoo.com>
2025-06-18 14:53:35 -0700

    hello world

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          testing deployment
+          Hello world, how are you?
         </p>
         <a
           className="App-link"
